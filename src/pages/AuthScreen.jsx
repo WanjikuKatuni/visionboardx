55e2f32c2d7758bd9974000e27c344ca2f4e50b5
wanjiku <annritawanjiku77@gmail.com>
2023-09-20 11:35:27 +0300

    created UI/UX for auth section and updated the theme

--- src/pages/AuthScreen.jsx
+++ src/pages/AuthScreen.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
-import {Container, Stack, OutlinedInput, Button, Typography} from '@mui/material'
-import logo from '../assets/vb.jpg'
+import React from "react";
+import { Container, Stack, TextField, Button, Typography } from "@mui/material";
+import logo from "../assets/vb.jpg";
+import ImageEl from "../components/utils/ImageEl";
 
 function AuthScreen() {
   return (
-    <Container>
+    <Container  maxWidth="xs" sx={{ mt: 10 }}>
+      <Stack mb={6} spacing={4} alignItems="center" textAlign="center">
         {/* logo */}
-        <img src={logo} alt='visionboard-logo' />
+        <ImageEl src={logo} alt="visionboard-logo" />
+        <Typography color="#C0C0C0">
+          Visualize your goals and dreams for increased motivation and
+          productivity. <br /> Access your vision board Anytime, Anywhere
+        </Typography>
+      </Stack>
+      {/* textfield */}
+      <Stack spacing={2}>
+        <TextField label="Email" />
+        <TextField label="Password" />
+      {/* buttons */}
+      <Button size="large" variant="contained">Login</Button>
+      </Stack>
+      <Typography mt={3} textAlign="center" color="#C0C0C0">Do not have an account?</Typography>
     </Container>
-  )
+  );
 }
 
-export default AuthScreen
+export default AuthScreen;
--- src/theme.js
+++ src/theme.js
@@ -7,7 +7,20 @@ const theme = createTheme({
         mode: "dark",
         background: {
             default: "#1d1f26"
+        },
+        primary:{
+            main: '#A36A00'
         }
+    },
+    typography: {
+        fontFamily: 'Lato, sans-serif',
+        button: {
+            textTransform: 'unset'
+            fontWeight: '700'
+        }
+    },
+    shape: {
+        borderRadius: 0
     }
 })
 
