d655dc591ff9ee5b435c1cba1b8c863d0ee85b43
wanjiku <annritawanjiku77@gmail.com>
2023-09-20 11:48:36 +0300

    added state for signup and login

--- src/pages/AuthScreen.jsx
+++ src/pages/AuthScreen.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Stack, TextField, Button, Typography } from "@mui/material";
 import logo from "../assets/vb.jpg";
 import ImageEl from "../components/utils/ImageEl";
 
 function AuthScreen() {
+  const [isLogin, setIsLogin] = useState(true);
+  const authText = isLogin
+    ? "Do not have an account?"
+    : "Already have an account?";
+
   return (
-    <Container  maxWidth="xs" sx={{ mt: 10 }}>
+    <Container maxWidth="xs" sx={{ mt: 10 }}>
       <Stack mb={6} spacing={4} alignItems="center" textAlign="center">
         {/* logo */}
         <ImageEl src={logo} alt="visionboard-logo" />
@@ -18,10 +23,21 @@ function AuthScreen() {
       <Stack spacing={2}>
         <TextField label="Email" />
         <TextField label="Password" />
-      {/* buttons */}
-      <Button size="large" variant="contained">Login</Button>
+        {/* buttons */}
+        <Button size="large" variant="contained">
+          {isLogin ? "Login" : "Sign Up"}
+        </Button>
       </Stack>
-      <Typography mt={3} textAlign="center" color="#C0C0C0">Do not have an account?</Typography>
+      <Typography
+    //   change from oldstate to newstate
+        onClick={() => setIsLogin((o) => !o)}
+        sx={{cursor: "pointer"}}
+        mt={3}
+        textAlign="center"
+        color="#C0C0C0"
+      >
+        {authText}
+      </Typography>
     </Container>
   );
 }
