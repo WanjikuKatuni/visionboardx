2fff382436b1630ad531036928f3c0d10de58bc9
wanjiku <annritawanjiku77@gmail.com>
2023-09-20 12:07:03 +0300

    added state for input elements

--- src/pages/AuthScreen.jsx
+++ src/pages/AuthScreen.jsx
@@ -3,11 +3,30 @@ import { Container, Stack, TextField, Button, Typography } from "@mui/material";
 import logo from "../assets/vb.jpg";
 import ImageEl from "../components/utils/ImageEl";
 
+// intiial form state
+const initForm = {
+    email: "",
+    password: "",
+}
+
 function AuthScreen() {
   const [isLogin, setIsLogin] = useState(true);
+
+  const [form, setForm] = useState(initForm)
+
   const authText = isLogin
     ? "Do not have an account?"
     : "Already have an account?";
+
+    
+    const handleChange = event => setForm((oldForm)=> ({
+        ...oldForm, [event.target.name]: event.target.value,
+    }))
+
+    const handleAuth = async () => {
+
+    }
+
 
   return (
     <Container maxWidth="xs" sx={{ mt: 10 }}>
@@ -21,10 +40,10 @@ function AuthScreen() {
       </Stack>
       {/* textfield */}
       <Stack spacing={2}>
-        <TextField label="Email" />
-        <TextField label="Password" />
+        <TextField onChange={handleChange} value={form.email} name="email" label="Email" />
+        <TextField onChange={handleChange} value={form.password} name="password" label="Password" />
         {/* buttons */}
-        <Button size="large" variant="contained">
+        <Button disabled={!form.email.trim() || !form.password.trim()} onClick={handleAuth} size="large" variant="contained">
           {isLogin ? "Login" : "Sign Up"}
         </Button>
       </Stack>
