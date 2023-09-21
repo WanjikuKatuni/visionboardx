59a84c7651de0a69be011e294e61e1db9de1f441
wanjiku <annritawanjiku77@gmail.com>
2023-09-21 03:25:19 +0300

    used react dnd to make the vision screen which will contain the droppables and the draggables

--- src/pages/VisionsScreen.jsx
+++ src/pages/VisionsScreen.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+// context of application
 
-function VisionsScreen() {
+
+import React from 'react'
+import {DragDropContext} from 'react-beautiful-dnd'
+
+export default function VisionsScreen() {
+
+    const [completed, setCompleted] = useState([])
+    const [incomplete, setIncomplete] = useState([])
+
   return (
-    <div>VisionsScreen</div>
+    <DragDropContext>
+        <h2 style={{textAlign: "center"}}>PROGRESS VISION BOARD </h2>
+
+        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row'}}>
+            {/* columns */}
+        </div>
+    </DragDropContext>
   )
 }
 
-export default VisionsScreen
